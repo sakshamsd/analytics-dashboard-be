49db552c365f6347a14ae077cb012951fd3fc8d4
sakshamsd <sakshamdangol04@gmail.com>
2026-04-09 19:40:18 +1000

    resolved build issue

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,20 @@
 import "dotenv/config";
 import { DataSource } from "typeorm";
+import { User } from "../entities/User.js";
+import { Workspace } from "../entities/Workspace.js";
+import { WorkspaceMember } from "../entities/WorkspaceMember.js";
+import { Company } from "../entities/Companies.js";
+import { Contact } from "../entities/Contact.js";
+import { Deals } from "../entities/Deals.js";
+import { Activities } from "../entities/Activities.js";
 
 const isProd = process.env.NODE_ENV === "production";
-
-// const isCompiled = __dirname.includes("dist");
-console.log("Migration DB config:", {
-	url: process.env.DATABASE_URL,
-});
 
 export const AppDataSource = new DataSource({
 	type: "postgres",
 	url: process.env.DATABASE_URL || "",
 	ssl: isProd ? { rejectUnauthorized: false } : false,
-	entities: ["src/entities/**/*.ts"],
+	entities: [User, Workspace, WorkspaceMember, Company, Contact, Deals, Activities],
 	migrations: ["src/migrations/*.ts"],
 	synchronize: false,
 });
